3432e75b7857033856029a11158dac57e21d3fe9
Kha Tran <kha.tran@lrz.de>
2024-04-25 21:26:32 +0200

    tabsize for html from 4 to 2

--- .config/nvim/ftplugin/html.lua
+++ .config/nvim/ftplugin/html.lua
@@ -1,6 +1,6 @@
 -- html indentation
 local opt = vim.o
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 vim.b.ale_linters = {'html-beautify'}
 vim.b.ale_fixers = {'html-beautify'}
